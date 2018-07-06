043f523ac732adbaf2296aafc7415ac05754172b
B00055 <chungyenlai@gmail.com>
2018-07-06 16:21:48 +0800

    改寫 if else成為switch

--- app/src/main/java/com/example/nommi/metronome/MainActivity.java
+++ app/src/main/java/com/example/nommi/metronome/MainActivity.java
@@ -13,37 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Get Background id number
-        textView = findViewById(R.id.background);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.background);
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
           //Get Detection for Action
-          if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                // Touch Screen
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
                 x1 = event.getX();
                 y1 = event.getY();
-          }
-          if(event.getAction() == MotionEvent.ACTION_UP) {
-               //Leave Screen
-               x2 = event.getX();
-               y2 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                //Computer and take
+                if(y1 - y2 > 50) {
+                    Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
+                    textView.setText("UP");
+                } else if(y2 - y1 > 50) {
+                    Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
+                    textView.setText("Down");
+                } else if(x1 - x2 > 50) {
+                    Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
+                    textView.setText("Left");
+                } else if(x2 - x1 > 50) {
+                    Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
+                    textView.setText("Right");
+                }
+                break;
+            case MotionEvent.ACTION_HOVER_ENTER:
+                break;
+            case MotionEvent.ACTION_HOVER_MOVE:
+                break;
+            case MotionEvent.ACTION_HOVER_EXIT:
+                break;
+        }
 
-               //Computer and take
-               if(y1 - y2 > 50) {
-                   Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
-                   textView.setText("UP");
-               } else if(y2 - y1 > 50) {
-                   Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
-                   textView.setText("Down");
-               } else if(x1 - x2 > 50) {
-                   Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
-                   textView.setText("Left");
-               } else if(x2 - x1 > 50) {
-                   Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
-                   textView.setText("Right");
-               }
-          }
+
           return super.onTouchEvent(event);
     }
 
