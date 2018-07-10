6c48644fe8ecf3f9c9258f7a5c1f8602908dd7f4
B00055 <chungyenlai@gmail.com>
2018-07-10 11:45:47 +0800

    新增motion_move

--- app/src/main/java/com/example/nommi/metronome/MainActivity.java
+++ app/src/main/java/com/example/nommi/metronome/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.nommi.metronome;
 
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     float x1 = 0; float x2 = 0; float y1 = 0; float y2 = 0;
+
+    PointF prePoint=new PointF(0,0);
+    PointF nowPoint=new PointF(0,0);
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,34 +24,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
           //Get Detection for Action
+
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                y1 = event.getY();
+                event.getActionMasked();
+                prePoint.x=event.getX();
+                prePoint.y=event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                nowPoint.x = event.getX();
+                nowPoint.y = event.getY();
+                Log.d("ACTION_MOVE",String.format("X=%f Y=%f",event.getX(),event.getY()));
+                //計算寫於此
+
+                //將座標記錄於此
+                prePoint.x=nowPoint.x;
+                prePoint.y=nowPoint.y;
                 break;
             case MotionEvent.ACTION_UP:
                 x2 = event.getX();
                 y2 = event.getY();
                 //Computer and take
-                if(y1 - y2 > 50) {
-                    Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
-                    textView.setText("UP");
-                } else if(y2 - y1 > 50) {
-                    Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
-                    textView.setText("Down");
-                } else if(x1 - x2 > 50) {
-                    Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
-                    textView.setText("Left");
-                } else if(x2 - x1 > 50) {
-                    Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
-                    textView.setText("Right");
-                }
-                break;
-            case MotionEvent.ACTION_HOVER_ENTER:
-                break;
-            case MotionEvent.ACTION_HOVER_MOVE:
-                break;
-            case MotionEvent.ACTION_HOVER_EXIT:
                 break;
         }
 
