6c67bb4e15730823c1c8cfc242d0cc309c7804b7
Nommi-PC\Nommi <whatsnommi@gmail.com>
2018-07-20 04:08:53 +0800

    2018.07.20 (1)ADD Tempo Name (2)ADD Tempo Value slection (3)ADD Tempo On/Off (4)ADD Start Action Function

--- app/src/main/java/com/example/nommi/metronome/MainActivity.java
+++ app/src/main/java/com/example/nommi/metronome/MainActivity.java
@@ -6,27 +6,44 @@ import android.text.style.BackgroundColorSpan;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     float x1 = 0; float x2 = 0; float y1 = 0; float y2 = 0;
-    //int tempo
+    //tempo name
     private TextView textView;
+    //tempo value
     private TextView textView1;
-    int number =60;
-        @Override
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                //Start to work  for Speed choice
+                ToggleButton b1 = (ToggleButton) findViewById(R.id.btn);
+                b1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+                    public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
+                        if (isChecked) {
+                            textView1 = findViewById(R.id.SpeedValue);
+                            String TempTotal = textView1.getText().toString();
+                            int IntTempTotal = Integer.parseInt(TempTotal);
+                            Toast.makeText(MainActivity.this, "start"+IntTempTotal, Toast.LENGTH_SHORT).show();
+                            // action start
+                        }
+                        else {
+                            // action stop
+                        }
+                    }
+                });
         }
                 @Override
                 public boolean onTouchEvent(MotionEvent event) {
                     //Get Background id number
                     textView = findViewById(R.id.background);
-                    textView1 = findViewById(R.id.SpeedValue);
 
-                    //textView1.setText(number);
                     //Get Detection for Action
                     if(event.getAction() == MotionEvent.ACTION_DOWN) {
                                 // Touch Screen
@@ -43,25 +60,141 @@ public class MainActivity extends AppCompatActivity {
                                 float x=Math.abs(x2-x1);
                                 float y=Math.abs(y2-y1);
                                 double z=Math.sqrt(x*x+y*y);
-                                //角度complete
+                                // angle computer
                                 int angle=Math.round((float)(Math.asin(y/z)/Math.PI*180));
-
-                                //Computer position and angle  take
+                                  //Computer position and angle  take
                                   if(y2 < y1  && angle>45) {
-                                    Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
-                                    textView.setText("UP");
+                                      textView1 = findViewById(R.id.SpeedValue);
+                                      String UpString = textView1.getText().toString();
+                                      int UpInt = Integer.parseInt(UpString);
+                                          if(UpInt <= 290 ) {
+                                              UpInt=UpInt+10;
+                                              textView1.setText(UpInt + "");
+                                              Toast.makeText(MainActivity.this, "向上滑" + UpInt, Toast.LENGTH_SHORT).show();
+
+                                              if (UpInt <= 20) {
+                                                  textView.setText("Laraghissimo");
+                                              } else if (UpInt <= 40) {
+                                                  textView.setText("Largamente");
+                                              } else if (UpInt <= 60) {
+                                                  textView.setText("Adagio");
+                                              } else if (UpInt <= 66) {
+                                                  textView.setText("Adagio");
+                                              } else if (UpInt <= 76) {
+                                                  textView.setText("Adagio");
+                                              } else if (UpInt <= 80) {
+                                                  textView.setText("Adagietto");
+                                              } else if (UpInt <= 108) {
+                                                  textView.setText("Andante");
+                                              } else if (UpInt <= 120) {
+                                                  textView.setText("Moderato");
+                                              } else if (UpInt <= 168) {
+                                                  textView.setText("Allegro");
+                                              } else if (UpInt <= 200) {
+                                                  textView.setText("Presto");
+                                              } else if (UpInt <= 300) {
+                                                  textView.setText("Prestissimo");
+                                              }
+                                          }
                                   } else if(y2 > y1 && angle>45 ) {
-                                    Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
-                                    textView.setText("Down");
+                                      textView1 = findViewById(R.id.SpeedValue);
+                                      String DownString = textView1.getText().toString();
+                                      int DownInt = Integer.parseInt(DownString);
+                                           //Value Check
+                                          if (DownInt >= 10) {
+                                              DownInt=DownInt-10;
+                                              textView1.setText(DownInt + "");
+                                              Toast.makeText(MainActivity.this, "向下滑" + DownInt, Toast.LENGTH_SHORT).show();
+                                                  if (DownInt <= 20) {
+                                                      textView.setText("Laraghissimo");
+                                                  } else if (DownInt <= 40) {
+                                                      textView.setText("Largamente");
+                                                  } else if (DownInt <= 60) {
+                                                      textView.setText("Adagio");
+                                                  } else if (DownInt <= 66) {
+                                                      textView.setText("Adagio");
+                                                  } else if (DownInt <= 76) {
+                                                      textView.setText("Adagio");
+                                                  } else if (DownInt <= 80) {
+                                                      textView.setText("Adagietto");
+                                                  } else if (DownInt <= 108) {
+                                                      textView.setText("Andante");
+                                                  } else if (DownInt <= 120) {
+                                                      textView.setText("Moderato");
+                                                  } else if (DownInt <= 168) {
+                                                      textView.setText("Allegro");
+                                                  } else if (DownInt <= 200) {
+                                                      textView.setText("Presto");
+                                                  } else if (DownInt <= 300) {
+                                                      textView.setText("Prestissimo");
+                                                  }
+                                          }
                                   } else if(x2 < x1 && angle <= 45) {
-                                    Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
-                                    textView.setText("Left");
+                                      textView1 = findViewById(R.id.SpeedValue);
+                                      String LeftString = textView1.getText().toString();
+                                      int LeftInt = Integer.parseInt(LeftString);
+                                              if (LeftInt<300) {
+                                                  LeftInt++;
+                                                  textView1.setText(LeftInt + "");
+                                                  Toast.makeText(MainActivity.this, "向左滑" + LeftInt, Toast.LENGTH_SHORT).show();
+                                                      if (LeftInt <= 20) {
+                                                          textView.setText("Laraghissimo");
+                                                      } else if (LeftInt <= 40) {
+                                                          textView.setText("Largamente");
+                                                      } else if (LeftInt <= 60) {
+                                                          textView.setText("Adagio");
+                                                      } else if (LeftInt <= 66) {
+                                                          textView.setText("Adagio");
+                                                      } else if (LeftInt <= 76) {
+                                                          textView.setText("Adagio");
+                                                      } else if (LeftInt <= 80) {
+                                                          textView.setText("Adagietto");
+                                                      } else if (LeftInt <= 108) {
+                                                          textView.setText("Andante");
+                                                      } else if (LeftInt <= 120) {
+                                                          textView.setText("Moderato");
+                                                      } else if (LeftInt <= 168) {
+                                                          textView.setText("Allegro");
+                                                      } else if (LeftInt <= 200) {
+                                                          textView.setText("Presto");
+                                                      } else if (LeftInt <= 300) {
+                                                          textView.setText("Prestissimo");
+                                                      }
+                                              }
                                   } else if(x2 > x1 && angle <= 45) {
-                                    Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
-                                    textView.setText("Right");
+                                      textView1 = findViewById(R.id.SpeedValue);
+                                      String RightString = textView1.getText().toString();
+                                      int RightInt = Integer.parseInt(RightString);
+                                              if (RightInt>0) {
+                                                  RightInt--;
+                                                  textView1.setText(RightInt + "");
+                                                  Toast.makeText(MainActivity.this, "向右滑" + RightInt, Toast.LENGTH_SHORT).show();
+                                                  if (RightInt <= 20) {
+                                                      textView.setText("Laraghissimo");
+                                                  } else if (RightInt <= 40) {
+                                                      textView.setText("Largamente");
+                                                  } else if (RightInt <= 60) {
+                                                      textView.setText("Adagio");
+                                                  } else if (RightInt <= 66) {
+                                                      textView.setText("Adagio");
+                                                  } else if (RightInt <= 76) {
+                                                      textView.setText("Adagio");
+                                                  } else if (RightInt <= 80) {
+                                                      textView.setText("Adagietto");
+                                                  } else if (RightInt <= 108) {
+                                                      textView.setText("Andante");
+                                                  } else if (RightInt <= 120) {
+                                                      textView.setText("Moderato");
+                                                  } else if (RightInt <= 168) {
+                                                      textView.setText("Allegro");
+                                                  } else if (RightInt <= 200) {
+                                                      textView.setText("Presto");
+                                                  } else if (RightInt <= 300) {
+                                                      textView.setText("Prestissimo");
+                                                  }
+                                              }
                                 }
                     }
                     return super.onTouchEvent(event);
                 }
-
     }
